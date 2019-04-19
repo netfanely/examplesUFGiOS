6e8d49a6e53b6a5b4dc32dc8b2777340a064eafc
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-04-19 09:19:29 -0500

    Add files via upload

---
+++ Exemplo5/Exemplo5/ViewController.swift
@@ -0,0 +1,24 @@
+/*************************************************************
+ Nome: Exemplo5
+ Descricao: exemplo demonstra uso de layouts em iOS
+ Autor: Silvano Malfatti
+ Data: 23/07/2018
+ Local: TCE
+ **************************************************************/
+
+//Bibliotecas utilizadas
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
